6c7b225cea2039a35535fbbedce03e7dc45d8e21
LeoNorn <hatabicib@gmail.com>
2023-11-08 21:21:51 +0600

    HW 7

--- parser_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cartoon_list_parser/', views.ParserCartoonListView.as_view()),
-    path('start_pars/', views.ParserFormView.as_view()),
-]
